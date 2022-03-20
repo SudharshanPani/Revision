d900412f9159d8c53af0ec074d3b6afa05cacec4
Sudharshan Pani <sudharshan.p@crediwatch.com>
2022-03-20 21:34:02 +0530

    added the counter application with bootstrap0

--- revision/src/App.js
+++ revision/src/App.js
@@ -1,8 +1,20 @@
+import React, { useState } from "react";
+// import './style.css'
 
 function App() {
+  const [num, setNum] = useState(0)
+
   return (
     <div className="App">
-      <h1>welcome to react</h1>
+      <h1 className="text-center" >Counter application</h1>
+      <div className=" d-flex justify-content-center align-items-center">
+
+        <button className="btn btn-primary" onClick={() => setNum(num + 1)}>+</button>
+        <p className="m-1" >{num}</p>
+        <button className="btn btn-primary" onClick={() => setNum(num - 1)}>-</button>
+
+      </div>
+
     </div>
   );
 }
